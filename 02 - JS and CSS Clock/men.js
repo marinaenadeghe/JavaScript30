cbc944f7237f7c887aa7b3be5a6426926187e330
Marina <marina.enadeghe@gmail.com>
2018-04-11 21:41:44 +0200

    02 Trennung

---
+++ 02 - JS and CSS Clock/men.js
@@ -0,0 +1,14 @@
+/**
+ * Schnipsel DOM geladen:
+ document.addEventListener("DOMContentLoaded", function(event) {
+    console.log("DOM fully loaded and parsed");
+  });
+ */
+
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    console.log("DOM fully loaded and parsed");
+
+
+
+});
