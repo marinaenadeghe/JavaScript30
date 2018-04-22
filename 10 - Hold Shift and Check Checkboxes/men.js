91edd5d82303f1acd16e8d2bd354ca2b24c935af
Marina <marina.enadeghe@gmail.com>
2018-04-22 20:17:17 +0200

    fkt wenn auch unklar wieso lastChecked= error

--- 10 - Hold Shift and Check Checkboxes/men.js
+++ 10 - Hold Shift and Check Checkboxes/men.js
@@ -13,10 +13,23 @@
      let lastChecked;
 
      function handleCheck (e){
-         console.log(e);
+         let inBetween = false;
+         if(e.shiftKey && this.checked){
+             checkboxes.forEach(checkbox => {
+                 console.log(checkbox) // zeigt alle an
+                 if(checkbox === this || checkbox === lastChecked){
+                     inBetween = !inBetween; // nicht false, da es auch rückwärts funktionieren soll
+                     console.log('check if inBetween')
+                 }
+                 if (inBetween){
+                     checkbox.checked = true;
+                 }
+             })
 
+         }
 
         lastChecked = this;
+        // wird richtig ausgegeben mit folgenden, jedoch nicht wenn getippt in Console: console.log(lastChecked)
      }
 
      checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck))
