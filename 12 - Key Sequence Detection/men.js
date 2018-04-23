4e9a364cf7a25d4106a69b540925309249b1a029
Marina <marina.enadeghe@gmail.com>
2018-04-23 16:42:58 +0200

    pressed.join('') in meiner Console falsch

--- 12 - Key Sequence Detection/men.js
+++ 12 - Key Sequence Detection/men.js
@@ -7,5 +7,22 @@
 
 
 document.addEventListener("DOMContentLoaded", event => {
-    console.log("DOM fully loaded and parsed");
+
+    let pressed = [];
+    let secretCode = 'marina';
+    window.addEventListener('keyup', (e) => {
+        console.log(e.key)
+        pressed.push(e.key)
+        pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length)
+        // wieso erhalte ich eine Fehlermeldung  mit "VM137:1 Uncaught ReferenceError: pressed is not defined
+        //     at <anonymous>:1:1" statt "osiMeavEnter" in console wenn ich pressed.join('') einfüge?
+        // wenn im HTML kommt "" statt obige Fehlermeldung
+        if(pressed.join('').includes(secretCode)) {
+            console.log('Ding, ding!!');
+            cornify.add()
+        }
+
+        console.log(pressed)
+})
+
 });
