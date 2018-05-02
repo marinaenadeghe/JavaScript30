badf22befdd10893656af71779d9ae392d57f38c
Marina <marina.enadeghe@gmail.com>
2018-05-02 21:13:51 +0200

    highlighting links - statt mit hover im css

--- 22 - Follow Along Link Highlighter/men.js
+++ 22 - Follow Along Link Highlighter/men.js
@@ -8,6 +8,18 @@ function highlightLink() {
     //console.log(this)
     let linkCoords = this.getBoundingClientRect()
     console.log(linkCoords)
+
+    let coord = {
+        width: linkCoords.width,
+        height: linkCoords.height,
+        x: linkCoords.x + window.scrollX, // ob x oder left nicht so wichtig, aber nötig, damit es auch bei scrollen geht
+        y: linkCoords.y + window.scrollY,
+    }
+
+
+    highlight.style.width = `${coord.width}px`
+    highlight.style.height = `${coord.height}px`
+    highlight.style.transform = `translate(${coord.x}px, ${coord.y}px)`
 }
 
 triggers.forEach(a => a.addEventListener('mouseenter', highlightLink))
